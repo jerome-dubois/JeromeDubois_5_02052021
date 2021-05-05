524845064f40c039a16a0e03c7fd035b3f529b2d
jerome-dubois <jerome.dubois@gmail.com>
2021-05-06 01:58:11 +0200

    Dynamic tests product.html

---
+++ main.js
@@ -0,0 +1,11 @@
+// const url = 'http://localhost:3000/api/cameras';
+
+// fetch(url)
+//   .then((resp) => resp.json())
+//   .then(function(data) {
+//      console.log(data);
+//   })
+//   .catch(function(error) {
+//      console.log(error);
+//   });
+
