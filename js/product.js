bac45cae98ef3a01e3d55e6a5d67c61432c86199
jerome-dubois <78096799+jerome-dubois@users.noreply.github.com>
2022-10-09 00:06:02 +0200

    Update product.js

--- js/product.js
+++ js/product.js
@@ -1,6 +1,11 @@
-var url_teddies = 'http://localhost:3000/api/teddies';
-var url_cameras = 'http://localhost:3000/api/cameras';
-var url_furniture = 'http://localhost:3000/api/furniture';
+let apiUrl =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? 'http://localhost:3000'
+    : 'https://mvp-site-ecommerce-orinoco.herokuapp.com'
+
+var url_teddies = '${apiUrl}/api/teddies';
+var url_cameras = '${apiUrl}/api/cameras';
+var url_furniture = '${apiUrl}/api/furniture';
 
 var url_array = [url_teddies, url_cameras, url_furniture];
 
