8add7b7781813f9f1d0d06689d3717ac8b7e81eb
jerome-dubois <78096799+jerome-dubois@users.noreply.github.com>
2022-10-09 11:17:00 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-let apiUrl =
- location.hostname === 'localhost' || location.hostname === '127.0.0.1'
- ? 'http://localhost:3000'
- : 'https://mvp-site-ecommerce-orinoco.herokuapp.com/'
+//let apiUrl =
+// location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+// ? 'http://localhost:3000'
+// : 'https://mvp-site-ecommerce-orinoco.herokuapp.com/'
